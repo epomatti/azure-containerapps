092b27ad5e27fb1210ad1334bc5edc6ad78dce47
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-09-23 11:33:01 -0300

    microservices

--- infra/main.tf
+++ infra/main.tf
@@ -65,84 +65,93 @@ resource "azapi_resource" "managed_environment" {
 
 ### Services ###
 
-# resource "azapi_resource" "container_app" {
-#   name      = "capps-service1"
-#   location  = azurerm_resource_group.default.location
-#   parent_id = azurerm_resource_group.default.id
-#   type      = "Microsoft.App/containerApps@2022-03-01"
+resource "azapi_resource" "service1" {
+  name      = "capps-service1"
+  location  = azurerm_resource_group.default.location
+  parent_id = azurerm_resource_group.default.id
+  type      = "Microsoft.App/containerApps@2022-03-01"
 
-#   body = jsonencode({
-#     properties : {
-#       managedEnvironmentId = azapi_resource.managed_environment.id
-#       configuration = {
-#         ingress = {
-#           external   = true
-#           targetPort = 3000
-#         }
-#       }
-#       template = {
-#         containers = [
-#           {
-#             name  = "service1"
-#             image = "epomatti/azure-containerapps-service1"
-#             resources = {
-#               cpu    = 0.5
-#               memory = "1.0Gi"
-#             }
-#           }
-#         ]
-#         scale = {
-#           minReplicas = 1
-#           maxReplicas = 2
-#         }
-#       }
-#     }
-#   })
-# }
+  response_export_values = ["properties.configuration.ingress.fqdn"]
 
-# resource "azapi_resource" "container_app" {
-#   name      = "capps-service2"
-#   location  = azurerm_resource_group.default.location
-#   parent_id = azurerm_resource_group.default.id
-#   type      = "Microsoft.App/containerApps@2022-03-01"
+  body = jsonencode({
+    properties : {
+      managedEnvironmentId = azapi_resource.managed_environment.id
+      configuration = {
+        ingress = {
+          external   = true
+          targetPort = 3000
+        }
+      }
+      template = {
+        containers = [
+          {
+            name  = "service1"
+            image = "epomatti/azure-containerapps-service1"
+            resources = {
+              cpu    = 0.5
+              memory = "1.0Gi"
+            }
+            env = [
+              { name = "SERVICE2_URL", value = "https://${jsondecode(azapi_resource.service2.output).properties.configuration.ingress.fqdn}" }
+            ]
+          }
+        ]
+        scale = {
+          minReplicas = 1
+          maxReplicas = 2
+        }
+      }
+    }
+  })
+}
 
-#   body = jsonencode({
-#     properties : {
-#       managedEnvironmentId = azapi_resource.managed_environment.id
-#       configuration = {
-#         ingress = {
-#           external   = true
-#           targetPort = 3100
-#         }
-#       }
-#       template = {
-#         containers = [
-#           {
-#             name  = "service2"
-#             image = "epomatti/azure-containerapps-service2"
-#             resources = {
-#               cpu    = 0.5
-#               memory = "1.0Gi"
-#             }
-#           }
-#         ]
-#         scale = {
-#           minReplicas = 1
-#           maxReplicas = 2
-#         }
-#       }
-#     }
-#   })
-# }
+resource "azapi_resource" "service2" {
+  name      = "capps-service2"
+  location  = azurerm_resource_group.default.location
+  parent_id = azurerm_resource_group.default.id
+  type      = "Microsoft.App/containerApps@2022-03-01"
+
+  response_export_values = ["properties.configuration.ingress.fqdn"]
+
+  body = jsonencode({
+    properties : {
+      managedEnvironmentId = azapi_resource.managed_environment.id
+      configuration = {
+        ingress = {
+          external   = true
+          targetPort = 3100
+        }
+      }
+      template = {
+        containers = [
+          {
+            name  = "service2"
+            image = "epomatti/azure-containerapps-service2"
+            resources = {
+              cpu    = 0.5
+              memory = "1.0Gi"
+            }
+          }
+        ]
+        scale = {
+          minReplicas = 1
+          maxReplicas = 2
+        }
+      }
+    }
+  })
+}
 
 
 ### Nginx Container ###
 
-resource "azapi_resource" "container_app" {
+resource "azapi_resource" "nginx" {
   name      = "capps-nginx"
   location  = azurerm_resource_group.default.location
   parent_id = azurerm_resource_group.default.id
   type      = "Microsoft.App/containerApps@2022-03-01"
+
+  response_export_values = ["properties.configuration.ingress.fqdn"]
 
   body = jsonencode({
     properties : {
@@ -171,4 +180,18 @@ resource "azapi_resource" "container_app" {
       }
     }
   })
+}
+
+### Outputs ###
+
+output "service1_fqdn" {
+  value = "https://${jsondecode(azapi_resource.service1.output).properties.configuration.ingress.fqdn}"
+}
+
+output "service2_fqdn" {
+  value = "https://${jsondecode(azapi_resource.service2.output).properties.configuration.ingress.fqdn}"
+}
+
+output "nginx_fqdn" {
+  value = "https://${jsondecode(azapi_resource.nginx.output).properties.configuration.ingress.fqdn}"
 }
